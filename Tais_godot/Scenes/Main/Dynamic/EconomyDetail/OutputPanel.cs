7665e7e200335d93ec284a3de6c2a6ff6e05b4c4
sericaer <sericaer@outlook.com>
2020-10-17 23:27:34 +0800

    修改显示

--- Tais_godot/Scenes/Main/Dynamic/EconomyDetail/OutputPanel.cs
+++ Tais_godot/Scenes/Main/Dynamic/EconomyDetail/OutputPanel.cs
@@ -7,17 +7,29 @@ namespace TaisGodot.Scripts
 	{
 		internal Output gmObj;
 
+		HSlider slider;
+
 		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
+			slider = GetNode<HSlider>("HSlider");
+
 			GetNode<Label>("Label").Text = gmObj.name;
-			GetNode<HSlider>("HSlider").Value = gmObj.percent.Value;
 			GetNode<ReactiveLabel>("Value").Assoc(gmObj.currValue);
+
+			slider.Value = gmObj.percent.Value;
+
+			slider.Connect("mouse_entered", this, nameof(UpdateTooltip));
 		}
 
 		private void _on_HSlider_value_changed(float value)
 		{
 			gmObj.percent.Value = value;
+		}
+
+		private void UpdateTooltip()
+		{
+			slider.HintTooltip = $"{gmObj.percent.Value}%";
 		}
 	}
 }
