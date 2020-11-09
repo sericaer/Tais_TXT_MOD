702c4f8b3e866030629b2d423dd3f71be57423f9
sericaer <sericaer@outlook.com>
2020-11-09 16:56:52 +0800

    修改实现

--- Tais_godot/Scenes/Main/Dynamic/EconomyDetail/EconomyDetailPanel.cs
+++ Tais_godot/Scenes/Main/Dynamic/EconomyDetail/EconomyDetailPanel.cs
@@ -19,9 +19,7 @@ namespace TaisGodot.Scripts
 		ReactiveLabel incomeTotal;
 		ReactiveLabel outputTotal;
 
-		AdjustPanel adjustPopTax;
-		AdjustPanel adjustReportChaoting;
-		AdjustPanel adjustAdminSpend;
+		VBoxContainer adjustContainer;
 
 		public static EconomyDetailPanel Instance(Node parent)
 		{
@@ -43,92 +41,18 @@ namespace TaisGodot.Scripts
 			incomeTotal.Assoc(GMRoot.runner.economy.incomeTotal);
 			outputTotal.Assoc(GMRoot.runner.economy.outputTotal);
 
-			adjustPopTax = GetNode<AdjustPanel>("");
-			adjustReportChaoting = GetNode<AdjustPanel>("");
-			adjustAdminSpend = GetNode<AdjustPanel>("");
-
-			adjustPopTax.Init(GMRoot.runner.adjusts.Single(x => x.etype == GMData.Run.Adjust.EType.POP_TAX));
-			adjustReportChaoting.Init(GMRoot.runner.adjusts.Single(x => x.etype == GMData.Run.Adjust.EType.REPORT_CHAOTING));
-			adjustAdminSpend.Init(GMRoot.runner.adjusts.Single(x => x.etype == GMData.Run.Adjust.EType.ADMIN_SPEND));
-
-			//foreach (var income in GMRoot.runner.economy.incomeAdjusts)
-			//{
-			//	var incomPanel = (IncomePanel)ResourceLoader.Load<PackedScene>("res://Scenes/Main/Dynamic/EconomyDetail/IncomePanel.tscn").Instance();
-			//	incomPanel.gmObj = income;
-
-			//	GetNode<VBoxContainer>("CenterContainer/EconomyDetail/VBoxContainer/HBoxContainer/Income/VBoxContainer/VBoxContainer").AddChild(incomPanel);
-			//}
-
-			//foreach (var output in GMRoot.runner.economy.outputAdjusts)
-			//{
-			//	var outputPanel = (OutputPanel)ResourceLoader.Load<PackedScene>("res://Scenes/Main/Dynamic/EconomyDetail/OutputPanel.tscn").Instance();
-			//	outputPanel.gmObj = output;
-
-			//	GetNode<VBoxContainer>("CenterContainer/EconomyDetail/VBoxContainer/HBoxContainer/Output/VBoxContainer/VBoxContainer").AddChild(outputPanel);
-			//}
-
-			////UpDateTempOutputStatus();
+			adjustContainer = GetNode<VBoxContainer>("");
+			foreach(GMData.Run.Adjust.EType elem in Enum.GetValues(typeof(GMData.Run.Adjust.EType)))
+            {
+				var adjustPanel = adjustContainer.GetNode<AdjustPanel>(elem.ToString());
+				adjustPanel.Init(GMRoot.runner.adjusts.Single(x => x.etype == elem));
+			}
 		}
 
 		public override void _ExitTree()
 		{
 			SpeedContrl.UnPause();
 		}
-
-		private void _on_Button_Confirm_pressed()
-		{
-			QueueFree();
-		}
-
-		private void _on_Button_Cancel_pressed()
-		{
-			QueueFree();
-		}
-
-		private void _onButtonFullFillCountryTax()
-		{
-			//var need = RunData.Chaoting.inst.expectYearTax.Value- RunData.Chaoting.inst.realYearTax.Value;
-
-			//RunData.Chaoting.inst.expectYearTax.Value = RunData.Chaoting.inst.realYearTax.Value;
-			//RunData.Economy.inst.curr.Value -= need;
-		}
-
-		private void UpDateTempOutputStatus()
-		{
-			UpdateFullFillCountryTax();
-		}
-
-		private void UpdateFullFillCountryTax()
-		{
-			//var btn = GetNode<Button>("ButtonFullFillCountryTax");
-
-			//var lackCountryTax = RunData.Chaoting.inst.expectYearTax.Value - RunData.Chaoting.inst.realYearTax.Value;
-			//if (lackCountryTax >= 0)
-			//{
-			//	btn.Disabled = false;
-			//	btn.HintTooltip = TranslateServerEx.Translate("STATIC_COUNTRY_TAX_NOT_LACK");
-			//}
-			//else if (RunData.Economy.inst.curr.Value < lackCountryTax)
-			//{
-			//	btn.Disabled = false;
-			//	btn.HintTooltip = TranslateServerEx.Translate("STATIC_COUNTRY_TAX_LACK_AND_ECONOMY_NOT_SUFFICENT",
-			//		lackCountryTax.ToString(),
-			//		RunData.Economy.inst.curr.Value.ToString());
-			//}
-			//else
-			//{
-			//	btn.Disabled = true;
-			//	btn.HintTooltip = TranslateServerEx.Translate("STATIC_COUNTRY_TAX_LACK_AND_ECONOMY_SUFFICENT",
-			//		lackCountryTax.ToString(),
-			//		RunData.Economy.inst.curr.Value.ToString());
-			//}
-		}
-
-		//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-		//  public override void _Process(float delta)
-		//  {
-		//      
-		//  }
 	}
 }
 
