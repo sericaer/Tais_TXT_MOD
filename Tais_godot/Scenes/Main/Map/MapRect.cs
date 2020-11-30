b0c795d2fd27958afc86d40b762607845c46c18a
sericaer <sericaer@outlook.com>
2020-11-30 15:31:46 +0800

    修改字体无法找到问题

--- Tais_godot/Scenes/Main/Map/MapRect.cs
+++ Tais_godot/Scenes/Main/Map/MapRect.cs
@@ -2,78 +2,78 @@ using Godot;
 
 namespace TaisGodot.Scripts
 {
-    public class MapRect : TextureRect
-    {
-        [Signal]
-        public delegate void MapClickSignal(int r, int g, int b);
+	public class MapRect : TextureRect
+	{
+		[Signal]
+		public delegate void MapClickSignal(int r, int g, int b);
 
-        public MapRect()
-        {
-            Texture = ResourceLoader.Load<Texture>("res://depart.png");
-        }
-        
-        // Declare member variables here. Examples:
-        // private int a = 2;
-        // private string b = "text";
+		public MapRect()
+		{
+			Texture = ResourceLoader.Load<Texture>("res://depart.png");
+		}
+		
+		// Declare member variables here. Examples:
+		// private int a = 2;
+		// private string b = "text";
 
-        public override void _Ready()
-        {
+		public override void _Ready()
+		{
 
-        }
+		}
 
-        
+		
 
-        private void _on_TextureRect_gui_input(object @event)
-        {
-            Color color;
-            if (GetMousePointColor(@event, out color))
-            {
-                EmitSignal(nameof(MapClickSignal), color.r8, color.g8, color.b8);
-            }
+		private void _on_TextureRect_gui_input(object @event)
+		{
+			Color color;
+			if (GetMousePointColor(@event, out color))
+			{
+				EmitSignal(nameof(MapClickSignal), color.r8, color.g8, color.b8);
+			}
 
-            // Replace with function body.
+			// Replace with function body.
 
-            //if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.IsPressed())
-            //{
-            //	GD.Print("Mouse Click/Unclick at: ", eventMouseButton.Position);
+			//if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.IsPressed())
+			//{
+			//	GD.Print("Mouse Click/Unclick at: ", eventMouseButton.Position);
 
-            //	var pos = eventMouseButton.Position;
+			//	var pos = eventMouseButton.Position;
 
-            //	var image = Texture.GetData();
-            //	image.Lock();
-            //	var color = image.GetPixel((int)pos.x, (int)pos.y);
-            //	image.Unlock();
+			//	var image = Texture.GetData();
+			//	image.Lock();
+			//	var color = image.GetPixel((int)pos.x, (int)pos.y);
+			//	image.Unlock();
 
-            //	GD.Print($"({color.r8}, {color.g8}, {color.b8})");
-            //}
+			//	GD.Print($"({color.r8}, {color.g8}, {color.b8})");
+			//}
 
-            //else if (@event is InputEventMouseMotion eventMouseMotion)
-            //	GD.Print("Mouse Motion at: ", eventMouseMotion.Position);
-        }
+			//else if (@event is InputEventMouseMotion eventMouseMotion)
+			//	GD.Print("Mouse Motion at: ", eventMouseMotion.Position);
+		}
 
-        private bool GetMousePointColor(object @event, out Color color)
-        {
-            if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.IsPressed())
-            {
-                var pos = eventMouseButton.Position;
+		private bool GetMousePointColor(object @event, out Color color)
+		{
+			if (@event is InputEventMouseButton eventMouseButton && eventMouseButton.IsPressed())
+			{
+				var pos = eventMouseButton.Position;
 
-                var image = Texture.GetData();
-                image.Lock();
-                color = image.GetPixel((int)pos.x, (int)pos.y);
-                image.Unlock();
+				var image = Texture.GetData();
+				image.Lock();
+				color = image.GetPixel((int)pos.x, (int)pos.y);
+				image.Unlock();
 
-                return true;
-            }
+				return true;
+			}
 
-            color = new Color();
-            return false;
-        }
+			color = new Color();
+			return false;
+		}
 
 
-        //  // Called every frame. 'delta' is the elapsed time since the previous frame.
-        //  public override void _Process(float delta)
-        //  {
-        //      
-        //  }
-    }
+		//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+		//  public override void _Process(float delta)
+		//  {
+		//      
+		//  }
+	}
 }
--- Tais_godot/Scenes/Main/Risk/RiskContainer.cs
+++ Tais_godot/Scenes/Main/Risk/RiskContainer.cs
@@ -9,14 +9,14 @@ namespace TaisGodot.Scripts
 {
 	class RiskContainer : HBoxContainer
 	{
-        public override void _Ready()
-        {
+		public override void _Ready()
+		{
 			GMRoot.runner._risks.Connect().OnItemAdded(_on_RiskObjAdd).Subscribe().EndWith(this);
 			GMRoot.runner._risks.Connect().OnItemRemoved(_on_RiskObjRemove).Subscribe().EndWith(this);
 		}
 
 		private void _on_RiskObjAdd(GMData.Run.Risk risk)
-        {
+		{
 			Risk.Instance(this, risk);
 		}
 
@@ -25,5 +25,5 @@ namespace TaisGodot.Scripts
 			var riskInst = this.GetChildren<Risk>().Single(x => x.gmObj == risk);
 			this.RemoveChild(riskInst);
 		}
-    }
+	}
 }
